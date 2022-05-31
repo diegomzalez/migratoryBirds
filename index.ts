49151381c4be0d96aa720c27f8935c68c0854524
Diego González <diegom.develops@gmail.com>
2022-05-30 23:29:55 -0400

    feat
    [ADD] problem solved and added documentation

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 import { migratoryBirds } from "./migratoryBirds";
-const result = migratoryBirds([5, 2, 3, 4, 0, 10, 20, 10, 40, 10]);
-console.log(result);
+const result = migratoryBirds([1, 1, 1, 1, 4, 4, 4, 4]);
+console.log("Smallest largest sighting is " + result);
--- migratoryBirds.ts
+++ migratoryBirds.ts
@@ -1,9 +1,19 @@
+/**
+ * This function will compute the smallest largest sighting in a group of sightings
+ * @param arr Sightings
+ * @returns The smallest largest sighting
+ */
 export function migratoryBirds(arr: number[]): number {
   const dict = setTypesAndValues(arr);
   const smallestSighting = getTheSmallestSighting(dict);
   return smallestSighting;
 }
 
+/**
+ * This function will compute how many times one sighting type is repeated
+ * @param arr Sighting types
+ * @returns The Sightings and how many time each is repeated
+ */
 function setTypesAndValues(arr: number[]): object {
   let dict: { [nameOfType: number]: number } = {};
   for (const type of arr) {
@@ -16,6 +26,11 @@ function setTypesAndValues(arr: number[]): object {
   return dict;
 }
 
+/**
+ * This function will find which sighting is the smallest largest
+ * @param dict The Sightings and how many time each is repeated
+ * @returns The smallest largest sighting
+ */
 function getTheSmallestSighting(dict: object): number {
   let smallestSightingType = 0;
   let smallestSightingMount = 0;
